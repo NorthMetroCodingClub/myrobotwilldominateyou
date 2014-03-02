e80f11618198942a3bae7b60cdb81793679222c9
Sam <phreakdawg@gmail.com>
2014-03-02 01:28:32 -0700

    crazy eyes even more crazy now yo

--- crazyeyes.py
+++ crazyeyes.py
@@ -2,22 +2,22 @@ import rg
 
 class Robot:
     def act(self, game):
-        # if we're in the center, stay put
-        if self.location == rg.CENTER_POINT:
-            return ['guard']
 
         # if there are >1 enemies around, suicide
         nhostiles = 0
+        enemylocs = []
+        enemyadjs = []
         for loc, bot in game.robots.iteritems():
             if bot.player_id != self.player_id:
-               
+                enemylocs.append(loc)
                 if rg.dist(loc, self.location) <= 1:
                     nhostiles = nhostiles + 1
+                    enemyadjs.append(loc);
                     #return ['attack', loc]
         if nhostiles > 1:
             return ['suicide']
         elif nhostiles == 1:
-            return ['guard']
+            return ['attack',enemyadjs.pop()]
         else:
             # move toward the center
-            return ['move', rg.toward(self.location, rg.CENTER_POINT)]
+            return ['move', rg.toward(self.location, enemylocs.pop())]
