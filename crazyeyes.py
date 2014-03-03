703fa273bf090eb8279e1dd138bf57a2d0a8ecbc
Sam <phreakdawg@gmail.com>
2014-03-02 23:23:46 -0700

    refined suicide ogic

--- crazyeyes.py
+++ crazyeyes.py
@@ -27,16 +27,27 @@ class Robot:
         nhostiles = 0
         enemylocs = []
         enemyadjs = []
+        enemyadjslocs = []
         for loc, bot in game.robots.iteritems():
             if bot.player_id != self.player_id:
                 enemylocs.append(loc)
                 if rg.wdist(loc, self.location) <= 1:
                     nhostiles = nhostiles + 1
-                    enemyadjs.append(loc);
-        if nhostiles > 1:
+                    enemyadjs.append(bot)
+                    enemyadjslocs.append(loc)
+
+        suicide_dmg_factor = 0
+        for x in enemyadjs:
+            if x.hp > rg.settings.suicide_damage:
+                suicide_dmg_factor += rg.settings.suicide_damage
+            else:
+                suicide_dmg_factor += x.hp
+        suicide_dmg_factor -= self.hp
+
+        if nhostiles > 1 and suicide_dmg_factor > 0:
             return ['suicide']
         elif nhostiles == 1:
-            return ['attack',enemyadjs.pop()]
+            return ['attack',enemyadjslocs.pop()]
         else:
             # move toward the center
             return ['move', rg.toward(self.location, self.strongest_enemy(game).location)]
